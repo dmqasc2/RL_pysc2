0ec976426f5df11402b2abede0894bea6507133d
dudwojae <46015392+dudwojae@users.noreply.github.com>
2019-08-13 23:37:19 +0900

    Update minigame.py

--- runs/minigame.py
+++ runs/minigame.py
@@ -1,3 +1,5 @@
+import time
+
 from pysc2.env import sc2_env
 from utils import arglist
 from copy import deepcopy
@@ -16,7 +18,7 @@ class MiniGame:
         self.nb_max_steps = 2000
         self.nb_episodes = nb_episodes
         self.env = sc2_env.SC2Env(map_name=self.map_name,
-                                  step_mul=16,
+                                  step_mul=2,
                                   visualize=False,
                                   agent_interface_format=[agent_format])
         self.learner = learner
@@ -24,6 +26,7 @@ class MiniGame:
 
     def run_ddpg(self, is_training=True):
         reward_cumulative = []
+        f = open("DDPG_result.txt", "w")
         for i_episode in range(self.nb_episodes):
             state = self.env.reset()[0]
             for t in range(self.nb_max_steps):  # Don't infinite loop while learning
@@ -34,19 +37,31 @@ class MiniGame:
                 # append memory
                 actions = self.preprocess.postprocess_action(actions)
                 self.learner.memory.append(obs, actions, state.reward, state.last(), training=is_training)
-                # self.learner.optimize()
 
                 if state.last():
+                    print(t)
+                    f = open("DDPG_result.txt", "a")
+                    last_re = state.reward
                     cum_reward = state.observation["score_cumulative"]
                     reward_cumulative.append(cum_reward[0])
+                    self.learner.optimize()
+                    f.write(f"reward: [{last_re}] score: [{cum_reward[0]}]\n")
                     break
                 else:
                     state = deepcopy(state_new)
+
+            time.sleep(0.5)
+
+            if (i_episode + 1) % 10000 == 0:
+                self.learner.save_models(fname=i_episode)
+
         self.env.close()
+        f.close()
         print(reward_cumulative)
 
     def run_ppo(self, is_training=True):
         reward_cumulative = []
+        f = open("PPO_result.txt", "w")
         for i_episode in range(self.nb_episodes):
             state = self.env.reset()[0]
             for t in range(self.nb_max_steps):  # Don't infinite loop while learning
@@ -59,13 +74,25 @@ class MiniGame:
                 self.learner.memory.append(obs, actions, state.reward, state.last(), training=is_training)
 
                 if state.last():
+                    f = open("PPO_result.txt", "a")
                     cum_reward = state.observation["score_cumulative"]
                     reward_cumulative.append(cum_reward[0])
+                    start = time.time()
                     self.learner.optimize(update=True)
                     self.learner.memory.clear()
+                    end = time.time()
+                    print(end-start)
+                    f.write(f"score: [{cum_reward[0]}]\n")
                     break
                 else:
                     state = deepcopy(state_new)
+
+            time.sleep(0.5)
+
+            if (i_episode + 1) % 10000 == 0:
+                self.learner.save_models(fname=i_episode)
+
         self.env.close()
+        f.close()
         print(reward_cumulative)
 
